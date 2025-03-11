b537b4e86fe05f3106fb6071dc7334fdd0ec19df
JoaoBoscoDuarte <joaoboscoduartef@gmail.com>
2025-03-11 00:54:06 -0300

    Update PasseioDeLancha.java

--- src/atv/turismo/src/compras/PasseioDeLancha.java
+++ src/atv/turismo/src/compras/PasseioDeLancha.java
@@ -18,6 +18,7 @@ public class PasseioDeLancha implements VendaDeTurismoIF {
          if (numeroDePessoas < 1) {
             throw new IllegalArgumentException("Deve ter no mínimo 1 pessoa!");
         }
+        
         this.numeroDePessoas = novaQtde;
     }
 
@@ -29,23 +30,29 @@ public class PasseioDeLancha implements VendaDeTurismoIF {
         if (qtdeDeHoras < 1 && qtdeDeHoras > 10) {
             throw new IllegalArgumentException("O tempo deve ser no mínimo 1 hora e no máximo 10h!");
         }
+        
         this.qtdeDeHoras = novaQtde;
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return Integer.hashCode(numeroDePessoas) + Integer.hashCode(qtdeDeHoras);
     }
 
     @Override
     public boolean equals(Object obj) {
-        return super.equals(obj);
+        if (this == obj) return true;
+        if (obj == null || getClass != obj.getClass()) return false;
+        PasseioDeLancha that = (PasseioDeLancha) obj;
+        return numeroDePessoas == that.numeroDePessoas && qtdeDeHoras == that.qtdeDeHoras;
     }
 
+    @Override
     public double getPreco() {
         return numeroDePessoas * qtdeDeHoras * PRECO_POR_PESSOA;
     }
 
+    @Override
     public String getDescricao() {
         return "Número de pessoas na viagem: " + numeroDePessoas + "\n" +
                 "Preço da viagem: " + getPreco() + "/n";
